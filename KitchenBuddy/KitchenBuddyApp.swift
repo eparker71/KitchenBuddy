5d30df89a09a7d4cac2b71b07e99d8140cf8efe0
Eric Parker <eparker71@gmail.com>
2025-04-17 15:35:19 -0400

    Initial Commit

---
+++ KitchenBuddy/KitchenBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenBuddyApp.swift
+//  KitchenBuddy
+//
+//  Created by Eric Parker on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct KitchenBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KitchenBuddyTests/KitchenBuddyTests.swift
@@ -0,0 +1,16 @@
+//
+//  KitchenBuddyTests.swift
+//  KitchenBuddyTests
+//
+//  Created by Eric Parker on 4/17/25.
+//
+
+import Testing
+
+struct KitchenBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
